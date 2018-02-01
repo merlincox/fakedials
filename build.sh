6df9e46a31ff198ecd4969fe1b432947bd583543
Merlin Cox <merlin.cox@denma.digital>
2018-02-01 18:21:37 +0000

    Remove .bin

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ TEMP_DIR=$(mktemp -d)
 SOURCE_DIR=$(dirname $0)
 TARGET_DIR=/usr/local/bin
 
-go build -o $TEMP_DIR/dialer.bin $SOURCE_DIR/dialer/main.go
-sudo mv -f $TEMP_DIR/dialer.bin $TARGET_DIR
-sudo chmod +x $TARGET_DIR/dialer.bin
+go build -o $TEMP_DIR/dialer $SOURCE_DIR/dialer/main.go
+sudo mv -f $TEMP_DIR/dialer $TARGET_DIR
+sudo chmod +x $TARGET_DIR/dialer
